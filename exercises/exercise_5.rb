3bd8c85d542d41b3c124e10b70561c094a1ce9bb
Jayden Shaw <jayden.shaw@gmail.com>
2018-12-04 19:14:16 +0000

    Exercise 5 Compelted.

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -13,3 +13,16 @@ puts "----------"
 # OUTPUT AVERAGE ANNUAL REVENUE FOR ALL STORES
 # OUTPUT STORES THAN ARE GENERATING OVER 1,000,000 IN SALES
     # CHAIN WHERE AND SIZE OR COUNT
+
+@stores_revenue = Store.sum('annual_revenue')
+@average_revenue = @stores_revenue / Store.count 
+@million_stores = Store.where('annual_revenue > 1000000')
+
+puts "----- Total Revenue -----"
+puts @stores_revenue
+
+puts "----- Average Revenue -----"
+puts @average_revenue
+
+puts "-----Million Dollar Stores-----"
+puts @million_stores

e98a237cef0669aec231b7fdf2141270e072b813
Jayden Shaw <jayden.shaw@gmail.com>
2018-12-04 18:23:58 +0000

    Exercise 2 Completed.

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -9,3 +9,8 @@ puts "----------"
 # LOAD FIRST STORE (ID=1) ASSIGN TO INSTANCE VARIABLE @store1
 # LOAD SECOND STORE (ID=2) ASSIGN TO @store2
 # UPDATE FIRST STORE CHANGING ITS NAME
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+@store1.name= 'Burnaby Clothing Warehouse'
+@store1.save
